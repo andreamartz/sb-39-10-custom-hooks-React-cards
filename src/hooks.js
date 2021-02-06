f6e7489816ad7cf48f2538cf37dc17b461da1138
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-02-06 17:32:50 -0600

    Add useAxios custom hook for PlayingCardList

--- src/hooks.js
+++ src/hooks.js
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import axios from "axios";
 
+// define useFlip
 const useFlip = (initialState = true) => {
   const [isFacingUp, setIsFacingUp] = useState(initialState);
+
   const flipCard = () => {
     setIsFacingUp(isUp => !isUp);
   };
+
   return [isFacingUp, flipCard];
 }
 
-export default useFlip;
+/**
+ * define useAxios:
+ *   * get data (a card) from an API
+ *   * use the data to update state (the cards)
+ */ 
+const useAxios = (baseUrl) => {
+  const [cards, setCards] = useState([]);
+  
+  // after the first render (and whenever url changes), get data from API and add to cards
+  const addCard = async () => {
+    try {
+      console.log("URL: ", baseUrl);
+      const res = await axios.get(baseUrl);
+      setCards(cards => [...cards, { ...res.data, id: uuid() }]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return [cards, addCard];
+}
+
+export { useFlip, useAxios };
